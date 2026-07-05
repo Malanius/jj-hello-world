0d015d8b52e950bf74c2188c4187d4f4d3d14cce
Michal Slota <malaniusprivierre@gmail.com>
2026-07-05 09:49:16 +0200

    feat: create hello and goodbye fns

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 // Still best one around yet!
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, cruel world!");
 }
